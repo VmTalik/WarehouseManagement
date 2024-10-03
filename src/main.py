9c92faad1fe2d2102a68b8582d7f975c60f7b7a2
VitaliiMarlian <vmarlyan@mail.ru>
2024-10-03 12:29:48 +0300

    Add code to run the application

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import uvicorn
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from core import db_helper, settings
+from api import router as api_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(api_router)
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:main_app",
+        host=settings.run_config.host,
+        port=settings.run_config.port,
+        reload=settings.run_config.reload
+    )
