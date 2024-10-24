3b66ecca2e77cef3bb521bc6d634c86abe041a69
VitaliiMarlian <vmarlyan@mail.ru>
2024-10-24 22:59:08 +0300

    Add StatusEnum class to __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,11 @@
 __all__ = (
     "Base",
-    "Order",
+    "Order", "StatusEnum",
     "OrderItem",
     "Product"
 )
 
 from .base import Base
-from .order import Order
+from .order import Order, StatusEnum
 from .order_item import OrderItem
 from .product import Product
