dd24ad3b8fb902beed537e4107f1bedae699e4a7
VitaliiMarlian <vmarlyan@mail.ru>
2024-11-04 19:37:56 +0300

    Add function to get paths to env files

--- src/core/config.py
+++ src/core/config.py
@@ -1,4 +1,5 @@
 import os
+from typing import Tuple
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -28,13 +29,19 @@ class TestDBConfig(DBConfig):
     pass
 
 
+def get_env_file_paths(*file_names: str) -> Tuple[str, ...]:
+    directory = os.path.dirname(os.path.abspath(__file__))
+    env_file_paths = tuple(os.path.join(directory, "../../", file_name) for file_name in file_names)
+    return env_file_paths
+
+
 class Settings(BaseSettings):
     db_config: DBConfig
     test_db_config: TestDBConfig
     engine_config: EngineConfig = EngineConfig()
     run_config: RunConfig = RunConfig()
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../", ".env"),
+        env_file=get_env_file_paths(".env.template", ".env"),
         case_sensitive=False,
         env_nested_delimiter="__"
     )
@@ -50,4 +57,3 @@ def get_db_url(test_db: bool = False) -> str:
 
     return (f"postgresql+asyncpg://{config.DB_USER}:{config.DB_PASSWORD}@"
             f"{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}")
-
